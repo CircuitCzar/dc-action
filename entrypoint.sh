298558dd9604d6ea18af068070d00ba5ee31c43c
bin <bin.zhou@datareachable.com>
2022-04-15 13:29:59 +0800

    🐛 fix bug 2022/04/15

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 set -e
 
 main() {
-    echo "" # see https://github.com/actions/toolkit/issues/168
-    echo ">> elgohr/Publish-Docker-Github-Action@master has been deprecated."
-    echo ">> Please use elgohr/Publish-Docker-Github-Action@v4 for a blast in speed and security."
+    echo "CICD-START"
 
+    # 检测是否有输入用户名和密码
     if usesBoolean "${ACTIONS_STEP_DEBUG}"; then
         echo "::add-mask::${INPUT_USERNAME}"
         echo "::add-mask::${INPUT_PASSWORD}"
         set -x
     fi
 
+    # 检测是否有输入项目名
     sanitize "${INPUT_NAME}" "name"
     if ! usesBoolean "${INPUT_NO_PUSH}"; then
         sanitize "${INPUT_USERNAME}" "username"
@@ -20,7 +20,7 @@ main() {
 
     registryToLower
     nameToLower
-
+    echo ${INPUT_REGISTRY}
     REGISTRY_NO_PROTOCOL=$(echo "${INPUT_REGISTRY}" | sed -e 's/^https:\/\///g')
     if uses "${INPUT_REGISTRY}" && ! isPartOfTheName "${REGISTRY_NO_PROTOCOL}"; then
         INPUT_NAME="${REGISTRY_NO_PROTOCOL}/${INPUT_NAME}"
@@ -28,12 +28,6 @@ main() {
 
     if uses "${INPUT_TAGS}"; then
         TAGS=$(echo "${INPUT_TAGS}" | sed "s/,/ /g")
-    else
-        translateDockerTag
-    fi
-
-    if uses "${INPUT_WORKDIR}"; then
-        changeWorkingDirectory
     fi
 
     if uses "${INPUT_USERNAME}" && uses "${INPUT_PASSWORD}"; then
@@ -44,22 +38,6 @@ main() {
     DOCKERNAME="${INPUT_NAME}:${FIRST_TAG}"
     BUILDPARAMS=""
     CONTEXT="."
-
-    if uses "${INPUT_DOCKERFILE}"; then
-        useCustomDockerfile
-    fi
-    if uses "${INPUT_BUILDARGS}"; then
-        addBuildArgs
-    fi
-    if uses "${INPUT_CONTEXT}"; then
-        CONTEXT="${INPUT_CONTEXT}"
-    fi
-    if usesBoolean "${INPUT_CACHE}"; then
-        useBuildCache
-    fi
-    if usesBoolean "${INPUT_SNAPSHOT}"; then
-        useSnapshot
-    fi
 
     build
 
@@ -79,6 +57,7 @@ main() {
     docker logout
 }
 
+# 判断参数是否为空
 sanitize() {
     if [ -z "${1}" ]; then
         echo >&2 "Unable to find the ${2}. Did you set with.${2}?"
@@ -86,10 +65,12 @@ sanitize() {
     fi
 }
 
+# registry转小写
 registryToLower() {
     INPUT_REGISTRY=$(echo "${INPUT_REGISTRY}" | tr '[A-Z]' '[a-z]')
 }
 
+# name转小写
 nameToLower() {
     INPUT_NAME=$(echo "${INPUT_NAME}" | tr '[A-Z]' '[a-z]')
 }
@@ -98,40 +79,8 @@ isPartOfTheName() {
     [ $(echo "${INPUT_NAME}" | sed -e "s/${1}//g") != "${INPUT_NAME}" ]
 }
 
-translateDockerTag() {
-    local BRANCH=$(echo "${GITHUB_REF}" | sed -e "s/refs\/heads\///g" | sed -e "s/\//-/g")
-    if hasCustomTag; then
-        TAGS=$(echo "${INPUT_NAME}" | cut -d':' -f2)
-        INPUT_NAME=$(echo "${INPUT_NAME}" | cut -d':' -f1)
-    elif isOnDefaultBranch; then
-        TAGS="latest"
-    elif isGitTag && usesBoolean "${INPUT_TAG_SEMVER}" && isSemver "${GITHUB_REF}"; then
-        if isPreRelease "${GITHUB_REF}"; then
-            TAGS=$(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g" | sed -E "s/v?([0-9]+)\.([0-9]+)\.([0-9]+)(-[a-zA-Z]+(\.[0-9]+)?)?/\1.\2.\3\4/g")
-        else
-            TAGS=$(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g" | sed -E "s/v?([0-9]+)\.([0-9]+)\.([0-9]+)/\1.\2.\3\4 \1.\2\4 \1\4/g")
-        fi
-    elif isGitTag && usesBoolean "${INPUT_TAG_NAMES}"; then
-        TAGS=$(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g")
-    elif isGitTag; then
-        TAGS="latest"
-    elif isPullRequest; then
-        TAGS="${GITHUB_SHA}"
-    else
-        TAGS="${BRANCH}"
-    fi
-}
-
 hasCustomTag() {
     [ $(echo "${INPUT_NAME}" | sed -e "s/://g") != "${INPUT_NAME}" ]
-}
-
-isOnDefaultBranch() {
-    if uses "${INPUT_DEFAULT_BRANCH}"; then
-        [ "${BRANCH}" = "${INPUT_DEFAULT_BRANCH}" ]
-    else
-        [ "${BRANCH}" = "master" ] || [ "${BRANCH}" = "main" ]
-    fi
 }
 
 isGitTag() {
@@ -140,27 +89,6 @@ isGitTag() {
 
 isPullRequest() {
     [ $(echo "${GITHUB_REF}" | sed -e "s/refs\/pull\///g") != "${GITHUB_REF}" ]
-}
-
-changeWorkingDirectory() {
-    cd "${INPUT_WORKDIR}"
-}
-
-useCustomDockerfile() {
-    BUILDPARAMS="${BUILDPARAMS} -f ${INPUT_DOCKERFILE}"
-}
-
-addBuildArgs() {
-    for ARG in $(echo "${INPUT_BUILDARGS}" | tr ',' '\n'); do
-        BUILDPARAMS="${BUILDPARAMS} --build-arg ${ARG}"
-        echo "::add-mask::${ARG}"
-    done
-}
-
-useBuildCache() {
-    if docker pull "${DOCKERNAME}" 2>/dev/null; then
-        BUILDPARAMS="${BUILDPARAMS} --cache-from ${DOCKERNAME}"
-    fi
 }
 
 uses() {
@@ -179,13 +107,6 @@ isPreRelease() {
     echo "${1}" | grep -Eq '-'
 }
 
-useSnapshot() {
-    local TIMESTAMP=$(date +%Y%m%d%H%M%S)
-    local SHORT_SHA=$(echo "${GITHUB_SHA}" | cut -c1-6)
-    local SNAPSHOT_TAG="${TIMESTAMP}${SHORT_SHA}"
-    TAGS="${TAGS} ${SNAPSHOT_TAG}"
-    echo "::set-output name=snapshot-tag::${SNAPSHOT_TAG}"
-}
 # 构建配置文件
 buildingConfiguration() {
     dockerConfig_1=''
@@ -200,7 +121,7 @@ buildingConfiguration() {
         dockerConfig_1="$dockerConfig_1$arg1$arg2$arg3"
         dockerConfig_2="${dockerConfig_2}WORKDIR /app/${file}\nRUN yarn install && yarn build\n"
         # nginx
-        location="\tlocation /${version} {\n"
+        location="\tlocation /v${version} {\n"
         talias="\t\talias   /usr/share/nginx/html_${version};\n"
         index="\t\tindex index.html index.htm;\n"
         try_files="\t\ttry_files \$uri \$uri/ /${version}/index.html;\n"
@@ -211,6 +132,7 @@ buildingConfiguration() {
     sed -i "5a \\${dockerConfig_2}" Dockerfile
     sed -i "29a \\${nginxConfig}" nginx.conf
 }
+# dockerfile构建项目
 build() {
     cp /Dockerfile ./
     cp /nginx.conf ./nginx.conf
@@ -218,52 +140,18 @@ build() {
 
     buildingConfiguration
 
-    cat nginx.conf
-
     local BUILD_TAGS=""
     for TAG in ${TAGS}; do
         BUILD_TAGS="${BUILD_TAGS}-t ${INPUT_NAME}:${TAG} "
     done
-    echo $INPUT_BUILDOPTIONS
-    echo "============"
-    echo $BUILDPARAMS
-    echo "============"
-    echo $BUILD_TAGS
-    echo "============"
-    echo $CONTEXT
-    docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} ${BUILD_TAGS} ${CONTEXT}
+    docker build ${BUILDPARAMS} ${BUILD_TAGS} ${CONTEXT}
 }
 
+# 发布包
 push() {
-    echo "============"
-    echo $TAGS
     for TAG in ${TAGS}; do
         docker push "${INPUT_NAME}:${TAG}"
     done
 }
 
 main
-# dockerfile构建项目
-# build() {
-#     cp /Dockerfile ./
-#     cp /nginx.conf ./nginx.conf
-#     cp /.dockerignore ./.dockerignore
-
-#     buildingConfiguration
-
-#     local BUILD_TAGS=""
-#     for TAG in ${TAGS}; do
-#         BUILD_TAGS="${BUILD_TAGS}-t ${INPUT_NAME}:${TAG} "
-#     done
-#     docker build ${BUILDPARAMS} ${BUILD_TAGS} ${CONTEXT}
-# }
-
-# # 发布包
-# push() {
-#     echo $TAGS
-#     for TAG in ${TAGS}; do
-#         docker push "${INPUT_NAME}:${TAG}"
-#     done
-# }
-
-# main
